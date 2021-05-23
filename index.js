f23dfd48ead8795da7c1ccc6e23ff85d116bf870
kaen <kusdsuna@naver.com>
2021-05-23 16:12:23 +0900

    firstSetup

---
+++ index.js
@@ -0,0 +1,27 @@
+const weatherBtn = document.querySelector(".weatherBtn");
+const sun = document.querySelector(".sun");
+const cloud = document.querySelector(".cloud");
+const rainnig = document.querySelector(".raining");
+const thunder = document.querySelector(".thunder");
+
+function onClickWeather(weather) {
+  weatherBtn.style.display = "none";
+  switch (weather) {
+    case "sun":
+      sun.style.display = "flex";
+      break;
+    case "cloud":
+      cloud.style.display = "flex";
+      break;
+    case "rainnig":
+      rainnig.style.display = "flex";
+      break;
+    case "thunder":
+      thunder.style.display = "flex";
+      break;
+  }
+}
+
+if (weatherBtn) {
+  weatherBtn.addEventListener("click", onClickWeather);
+}
